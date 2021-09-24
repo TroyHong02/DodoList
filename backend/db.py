453e8f19a1397bdb399589c20787715726a1cb99
e9510cba <e9510cba@pm.me>
2021-09-24 00:29:15 -0700

    ok fix mongodb schema fr

--- backend/db.py
+++ backend/db.py
@@ -15,7 +15,6 @@ users {
 lists = db['lists']
 """
 lists {
-    owner: objectId
     list_name: str
     tasks: [objectIds]
 }
@@ -26,40 +25,47 @@ tasks = db['tasks']
 tasks {
     task_name: str
     task_desc: str
+    inserted_time: time
     due_date: time
     status: str
-    list: objectId
-    owner: objectId # TODO needed?
 }
 """
 
 
 # TODO can merge new_list and update list to update with upsert = true
 # unique list name as key..
+
+# TODO 2 get list id before insert, then update user once, instead of finding -> list insert -> update
 def new_list(email, list_name, tasks):
+
     user = users.find_one({'email': email})
     if not user:
         return {'success': False, 'message': 'user not found'}
-    
-    owner = user['_id']
+
+    print(user)
+    user_id = user['_id']
 
     res = lists.insert_one({
-        'owner': owner,
         'list_name': list_name,
         'tasks': tasks,
     })
 
+    _id = res.inserted_id
+
+    # TODO unique array needed?
+    users.update({'_id': user_id}, {
+        '$push': { 'lists': _id }
+    })
+
     return {'success': True, 'message': 'new list created'}
 
-def get_list(email, list_name):
+def get_list(email, id):
 
     user = users.find_one({'email': email})
     if not user:
         return {'success': False, 'message': 'user not found'}
     
-    owner = user['_id']
-
-    res = lists.find_one({'owner': owner, 'list_name': list_name})
+    res = lists.find_one({'_id': id})
     # if not res:
     #     return {'success': False, 'message': 'list not found'}
 
@@ -70,10 +76,14 @@ def get_lists(email):
     user = users.find_one({'email': email})
     if not user:
         return {'success': False, 'message': 'user not found'}
+
+    print(user)
+
+    res = [None] * len(user['lists'])
+
+    for i in range(len(user['lists'])):
+        _id = user['lists'][i]
+        res[i] = lists.find_one({'_id': _id})
     
-    owner = user['_id']
-
-    res = lists.find({'owner': owner})
-
     return {'success': True, 'data': list(res)}
     
--- backend/test_db.py
+++ backend/test_db.py
@@ -2,6 +2,15 @@ import db
 
 db.new_list('username@example.com', 'yyy', [])
 
-print(db.get_lists('username@example.com'))
+res = db.get_lists('username@example.com')
 
-print(db.get_list('username@example.com', 'yyy'))
+print(res)
+
+lists = res['data']
+
+for lst in lists:
+    print(lst)
+
+lst1_id = lst['_id']
+
+print(db.get_list('username@example.com', lst1_id))
